847385f02e77d32e675c3f4fcf4f1902496d6769
ahwelgemoed <arno.welgemoed@outlook.com>
2021-08-03 09:52:33 +0200

    .

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
-const { spawn } = require("child_process");
+const fs = require("fs");
+const path = require("path");
 
-// const result = await spawnAsync("npm", ["build", "--prefix", widgetStructure.base], {
-//     cwd: process.cwd(),
-//     env: process.env,
-//     stdio: "pipe"
-// });
-// var savedOutput = result.stdout;
+const getAllFiles = function(dirPath, arrayOfFiles) {
+    files = fs.readdirSync(dirPath);
 
-// console.log(String(savedOutput));
-// return savedOutput;
+    arrayOfFiles = arrayOfFiles || [];
 
-(async () => {
-    const exitCode = spawn("npm run build --prefix ~/Documents/repos/app-guide-mendix-widget/", {
-        shell: true
+    files.forEach(function(file) {
+        if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+            arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
+        } else {
+            arrayOfFiles.push(path.join(__dirname, dirPath, file));
+        }
     });
 
-    console.log(`Starting Script`, exitCode.stdout);
-    // Setup DB
-})();
+    return arrayOfFiles;
+};
+
+const convertBytes = function(bytes) {
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+
+    if (bytes == 0) {
+        return "n/a";
+    }
+
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+
+    if (i == 0) {
+        return bytes + " " + sizes[i];
+    }
+
+    return (bytes / Math.pow(1024, i)).toFixed(1) + " " + sizes[i];
+};
+
+const getTotalSize = function(directoryPath) {
+    const arrayOfFiles = getAllFiles(directoryPath);
+
+    let totalSize = 0;
+
+    arrayOfFiles.forEach(function(filePath) {
+        totalSize += fs.statSync(filePath).size;
+    });
+
+    return convertBytes(totalSize);
+};
+
+const result = getTotalSize("./dist");
+console.log(`result`, result);
